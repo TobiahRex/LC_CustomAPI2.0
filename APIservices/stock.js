240e8dc8e3b043be2f6999848bb5f3818abeb48f
RexCorpDev <rexcorpfx@gmail.com>
2016-06-15 18:26:24 -0700

    stock API implemented

---
+++ APIservices/stock.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const NPMreq = require('request');
+
+let Stock = {
+  get(symbol, cb){
+    NPMreq(`http://dev.markitondemand.com/Api/v2/Quote/json?symbol=${symbol}`, (err, res, body) => {
+      if(err || res.statusCode !== 200) return cb(err || `ERROR: ${res.statusCode}`);
+      return cb(null, res.statusCode, body);
+    });
+  }
+}
+
+module.exports = Stock;
