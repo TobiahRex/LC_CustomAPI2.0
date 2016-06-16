6c03c97b408d12cbb2c8f24ecbff1ac95d9f93f7
RexCorpDev <rexcorpfx@gmail.com>
2016-06-15 18:08:09 -0700

    math services completed

--- APIservices/math.js
+++ APIservices/math.js
@@ -1,17 +1,29 @@
 'use strict';
 
 let MATH = {
-  sum(nums){
-    return nums.split('').map(Number).reduce((tot, num) => tot + num);
-  },
   square(num){
     return Math.pow(parseInt(num), 2);
   },
   cube(num){
     return Math.pow(parseInt(num), 3);
   },
+  power(nums){
+    return Math.pow(parseInt(nums[2]), nums[3]);
+  },
   root(num){
     return Math.sqrt(parseInt(num));
+  },
+  sum(nums){
+    return nums.split('').map(Number).reduce((tot, num) => tot + num);
+  },
+  subtract(nums){
+    return nums[2] - nums[3];
+  },
+  multiply(nums){
+    return nums[2]*nums[3];
+  },
+  div(nums){
+    return nums[2]/nums[3];
   }
 };
 
