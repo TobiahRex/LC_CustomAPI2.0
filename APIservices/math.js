d95b87ac08741df4b2c41b2929569ac05972031e
RexCorpDev <rexcorpfx@gmail.com>
2016-06-15 17:37:28 -0700

    math functions complete

--- APIservices/math.js
+++ APIservices/math.js
@@ -3,6 +3,15 @@
 let MATH = {
   sum(nums){
     return nums.split('').map(Number).reduce((tot, num) => tot + num);
+  },
+  square(num){
+    return Math.pow(parseInt(num), 2);
+  },
+  cube(num){
+    return Math.pow(parseInt(num), 3);
+  },
+  root(num){
+    return Math.sqrt(parseInt(num));
   }
 };
 
--- server.js
+++ server.js
@@ -1,30 +1,39 @@
 'use strict';
 
-const PORT = 8000;
-const http = require('http');
+const PORT    = 3000;
+const http    = require('http');
+const fs      = require('fs');
+const path    = require('path');
 const MathAPI = require('./APIservices/math.js');
 
 const server = http.createServer((req, res) => {
-
+  res.write(fs.readFileSync(path.join(__dirname, 'APIservices/description.txt'), 'utf8'));
+  let params = req.url.split('/');
+  console.log(params);
   console.log(req.url);
 
-  let params = req.url.split('/');
 
-  // switch(params[2]){
-  //   case 'sum' : res.write(`SUM = ${MathAPI.sum(params[3])}`)
-  // }
-
-
-
-
-
-
-
-
-
-
-
-
+  switch(params[1]){
+    case 'square' :
+      res.write(`\n\nYour request = ${req.url}\n\nSQUARE = ${MathAPI.square(params[2])}`);
+      res.end();
+      break;
+    case 'sum'    :
+      res.write(`\n\nYour request = ${req.url}\n\nSUM = ${MathAPI.sum(params[2])}`);
+      res.end();
+      break;
+    case 'cube'   :
+      res.write(`\n\nYour request = ${req.url}\n\nCUBE = ${MathAPI.cube(params[2])}`);
+      res.end();
+      break;
+    case 'root'   :
+      res.write(`\n\nYour request = ${req.url}\n\nSQUARE ROOT = ${MathAPI.root(params[2])}`);
+      res.end();
+      break;
+    default       :
+    res.write(`${params} : Does not Match Any Services Available.`);
+    res.end();
+  };
 
 });
 server.listen(PORT, err => {
