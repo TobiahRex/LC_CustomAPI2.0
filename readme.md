1abe7e0993d62ad947b51b9294755d3484261bf7
RexCorpDev <rexcorpfx@gmail.com>
2016-06-15 18:36:16 -0700

    readme update

--- readme.md
+++ readme.md
@@ -2,15 +2,22 @@
 * Demonstration of how to build your own API using Node HTTP services.
 
 ## Features Available
-* Do Basic Math.
-  - Sums.
-  - Squares.
-  - Cubes.
-  - Square Root.
-  - Division.
-* Get Date Info.
-  - Current Time.
-  - Current Date.
-  - Days till X.
+* Math API  :
+  - Sum         = '/sum/123'     -> 6
+  - Subtraction = '/minus/2/4'   -> -2
+  - Multiply    = '/multiply/2/5'-> 10
+  - Division    = '/divide/6/2'  -> 3
+  - Square      = '/square/2'    -> 4
+  - Cube        = '/cube/2'      -> 8
+  - Square Root = '/root/49'     -> 7
+  - Nth Power   = '/power/2/4'   -> 16
+* Date API :
+  - Current Time = '/now'        -> 5:48:37 PM
+  - Current Date = '/today'      -> June 15, 2016
+
 * Get Stock Info
-  - Stock Information based on Symbol.
+  - Stock = '/quote/APPL'
+
+* Get Time You've been Alive
+  Note: Give birthday, and you see how long you've been alive.
+ - Alive = '/alive/yy/mm/dd'
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const PORT    = 3000;
-const http    = require('http');
-const fs      = require('fs');
-const path    = require('path');
-const MathAPI = require('./APIservices/math.js');
+const PORT      = 3000;
+const http      = require('http');
+const fs        = require('fs');
+const path      = require('path');
+const MathAPI   = require('./APIservices/math.js');
+const DateAPI   = require('./APIservices/date.js');
+const StockAPI  = require('./APIservices/stock.js');
 
 const server = http.createServer((req, res) => {
   res.write(fs.readFileSync(path.join(__dirname, 'APIservices/description.txt'), 'utf8'));
@@ -12,27 +14,60 @@ const server = http.createServer((req, res) => {
   console.log(params);
   console.log(req.url);
 
-
   switch(params[1]){
     case 'square' :
-      res.write(`\n\nYour request = ${req.url}\n\nSQUARE = ${MathAPI.square(params[2])}`);
-      res.end();
-      break;
-    case 'sum'    :
-      res.write(`\n\nYour request = ${req.url}\n\nSUM = ${MathAPI.sum(params[2])}`);
-      res.end();
-      break;
+    res.write(`\n\nYour request = ${req.url}\n\nSQUARE = ${MathAPI.square(params[2])}`);
+    res.end();
+    break;
     case 'cube'   :
-      res.write(`\n\nYour request = ${req.url}\n\nCUBE = ${MathAPI.cube(params[2])}`);
+    res.write(`\n\nYour request = ${req.url}\n\nCUBE = ${MathAPI.cube(params[2])}`);
+    res.end();
+    break;
+    case 'root'   :
+    res.write(`\n\nYour request = ${req.url}\n\nSQUARE ROOT = ${MathAPI.root(params[2])}`);
+    res.end();
+    break;
+    case 'power'  :
+    res.write(`\n\nYour request = ${req.url}\n\nNTH POWER = ${MathAPI.power(params)}`);
+    res.end();
+    break;
+    case 'sum'    :
+    res.write(`\n\nYour request = ${req.url}\n\nSUM = ${MathAPI.sum(params[2])}`);
+    res.end();
+    break;
+    case 'minus'  :
+    res.write(`\n\nYour request = ${req.url}\n\nDIFFERENCE = ${MathAPI.subtract(params)}`);
+    res.end();
+    break;
+    case 'multiply':
+    res.write(`\n\nYour request = ${req.url}\n\nPRODUCT = ${MathAPI.multiply(params)}`);
+    res.end();
+    break;
+    case 'divide' :
+      res.write(`\n\nYour request = ${req.url}\n\nQUOTIENT = ${MathAPI.div(params)}`);
       res.end();
       break;
-    case 'root'   :
-      res.write(`\n\nYour request = ${req.url}\n\nSQUARE ROOT = ${MathAPI.root(params[2])}`);
+    case 'now'    :
+      res.write(`\n\nYour request = ${req.url}\n\nCURRENT TIME = ${DateAPI.now()}`);
       res.end();
+      break;
+    case 'today'  :
+      res.write(`\n\nYour request = ${req.url}\n\nCURRENT DATE = ${DateAPI.today()}`);
+      res.end();
+      break;
+    case 'quote'  :
+      StockAPI.get(params[2], (err, status, info) => {
+        if(err || status !== 200) {
+          res.write(`ERROR: That Symbol (${params[2]}) Does Not Exist. Try Again.`);
+          res.end();
+        }
+        res.write(`STOCK INFO: \n${info}`);
+        res.end();
+      });
       break;
     default       :
-    res.write(`${params} : Does not Match Any Services Available.`);
-    res.end();
+      res.write(`${params} : Does not Match Any Services Available.`);
+      res.end();
   };
 
 });
